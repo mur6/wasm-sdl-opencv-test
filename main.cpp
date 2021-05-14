0189ac1e8eace65a159c0a4a8527b53d05a18958
MURAKI Taichi <muraki.taichi@gmail.com>
2021-05-14 10:46:01 +0900

    Fix to show the image processed by open cv on the canvas

--- main.cpp
+++ main.cpp
@@ -4,41 +4,51 @@
 
 #include <SDL/SDL.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <emscripten.h>
 #include <emscripten/bind.h>
 
-//extern "C" int main(int argc, char **argv) {
-//  return 0;
-//}
 
-void image_output() {
-  constexpr int WIDTH = 128;//cols
-  constexpr int HEIGHT = 32;//rows
-  SDL_Surface *screen = nullptr;
-  SDL_Init(SDL_INIT_VIDEO);
-  screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
+cv::Mat processImage(const cv::Mat& input) {
+    cv::Mat gray, blur, detectedEdges;
+    // gray
+    cv::cvtColor(input, gray, cv::COLOR_RGB2GRAY);
+    // blur
+    double sigma = 0.91641;
+    int filterLength = 9;
+    GaussianBlur(gray, blur, cv::Size(filterLength, filterLength), sigma, sigma, cv::BORDER_REPLICATE);
+    // canny edge
+    double canny_lowThreshold=100, canny_highThreshold=200;
+    Canny(blur, detectedEdges, canny_lowThreshold, canny_highThreshold, 3);
+    return detectedEdges;
+}
 
-  if (SDL_MUSTLOCK(screen))
-    SDL_LockSurface(screen);
-   cv::Mat A(HEIGHT, WIDTH, CV_8UC4);
-   A.setTo(cv::Scalar(30,200,0));
-   std::cout << "A=" << A << std::endl;
-   std::cout << "width=" << A.size().width << ", height=" << A.size().height << std::endl;
-  cv::Mat B(A.size().height, A.size().width, CV_8UC4, screen->pixels);
-  A.copyTo(B);
-  std::cout << "B=" << B << std::endl;
-//   for (int i = 0; i < 32; i++) {
-//     for (int j = 0; j < 32; j++) {
-//       int alpha = 255;
-//       *((Uint32*)screen->pixels + i * 32 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
-//     }
-//   }
-  //std::cout << "DoValidateImageOnlyCanny screen->pixels=" << screen->pixels << std::endl;
-  if (SDL_MUSTLOCK(screen))
-    SDL_UnlockSurface(screen);
-  SDL_Flip(screen);
+void image_input(int offset, size_t size, int width, int height) {
+    uint8_t *pos;
+    pos = reinterpret_cast<uint8_t *>(offset);
+    cv::Mat inputImage(height, width, CV_8UC4, pos);
+
+    //std::cout << "INPUT: width=" << width << ", height=" << height << std::endl;
+
+    SDL_Surface *screen = nullptr;
+    SDL_Init(SDL_INIT_VIDEO);
+    screen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE);
+
+    if (SDL_MUSTLOCK(screen))
+        SDL_LockSurface(screen);
+
+
+    cv::Mat processed = processImage(inputImage);
+
+    cv::Mat outputCanvasImage(inputImage.size().height, inputImage.size().width, CV_8UC4, screen->pixels);
+
+    cv::cvtColor(processed, outputCanvasImage, cv::COLOR_GRAY2RGBA);
+    //std::cout << "outputCanvasImage=" << outputCanvasImage << std::endl;
+    if (SDL_MUSTLOCK(screen))
+        SDL_UnlockSurface(screen);
+    SDL_Flip(screen);
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
-  emscripten::function("image_output", &image_output);
+    emscripten::function("image_input", &image_input);
 }
